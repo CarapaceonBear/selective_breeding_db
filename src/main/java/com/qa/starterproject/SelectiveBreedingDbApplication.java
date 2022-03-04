8ef4f8e1cc724ef2a400c4b82554ba2341fef18b
carapaceonbear <seerhan17@gmail.com>
2022-03-04 10:15:34 +0000

    Initial commit of folder structure

---
+++ src/main/java/com/qa/starterproject/SelectiveBreedingDbApplication.java
@@ -0,0 +1,13 @@
+package com.qa.starterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectiveBreedingDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelectiveBreedingDbApplication.class, args);
+	}
+
+}
