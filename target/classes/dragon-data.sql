5305956c2498a1fb99b38781a7fd64e3722d27ec
Will Smith <97603686+CarapaceonBear@users.noreply.github.com>
2022-03-11 14:15:08 +0000

    Delete target directory, should be caught in .gitignore

--- target/classes/dragon-data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO dragon (name, sex, generation, colour, scale_quality, flying_speed, egg_size, egg_quality, breath_temperature)
-			VALUES ('Example1', 'Male', 1, 'Red', 1.0, 1.0, 1.0, 1.0, 1.0);
-INSERT INTO dragon (name, sex, generation, colour, scale_quality, flying_speed, egg_size, egg_quality, breath_temperature)
-			VALUES ('Example2', 'Female', 1, 'Blue', 2.0, 2.0, 2.0, 2.0, 2.0);
-INSERT INTO dragon (name, sex, generation, colour, scale_quality, flying_speed, egg_size, egg_quality, breath_temperature)
-			VALUES ('Example3', 'Male', 2, 'Green', 3.0, 3.0, 3.0, 3.0, 3.0);
-INSERT INTO dragon (name, sex, generation, colour, scale_quality, flying_speed, egg_size, egg_quality, breath_temperature)
-			VALUES ('Example4', 'Female', 2, 'Red', 4.0, 4.0, 4.0, 4.0, 4.0)
